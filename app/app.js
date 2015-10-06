8680737c255ae5ed0f8c070e015df4f7a0dc8071
Brad Murphy <brad.murphy@byhook.com>
2015-10-06 12:21:30 -0400

    Added animation for views.

--- app/app.js
+++ app/app.js
@@ -1,6 +1,29 @@
 'use strict';
 
+// require TweenMax
+var TweenMax = require('TweenMax');
+
 // initialize app
-var app = angular.module('folioApp', ['ngRoute']);
+var app = angular.module('folioApp', ['ngRoute', 'ngAnimate']);
+
+// animation for views
+app.animation('.content', function() {
+
+  return {
+
+    enter: function(element, done) {
+
+      TweenMax.from(element, 0.4, {
+        x: '100%',
+        autoAlpha: 0,
+        ease: Power4.easeOut,
+        onComplete: done()
+      });
+
+    }
+
+  };
+
+});
 
 module.exports = app;
