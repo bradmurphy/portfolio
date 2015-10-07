da32401f166e4e9199bcbd2d573da00da29ed036
Brad Murphy <brad.murphy@byhook.com>
2015-10-07 13:46:49 -0400

    Updated typography, removed unused config data for map

--- app/config.js
+++ app/config.js
@@ -8,16 +8,6 @@ var config = {
   ],
 
   data: {
-    name: 'Ann Arbor',
-    address: '11770 Haynes Bridge Road',
-    cityState: 'Alpharetta, GA 30009',
-    telLink: '6787445844',
-    tel: '678.744.5844',
-    dir: 'https://www.google.com/maps/dir/Current+Location/34.069504,-84.295151',
-    web: 'http://salonsbyjc.com/alpharetta-alpharetta-crossing/',
-    fb: 'https://www.facebook.com/pages/Salons-by-JC-Alpharetta/876484342370968',
-    ig: 'https://instagram.com/salonsbyjc_alpharetta/',
-    contact: 'http://salonsbyjc.com/alpharetta-alpharetta-crossing/contact-us/',
     lat: 42.279594,
     lng: -83.732124,
     marker: 'images/alpharetta.png'
