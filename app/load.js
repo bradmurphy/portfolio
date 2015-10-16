3feb115f5c6da76b68a40d7e1441af84a2f8c833
Brad Murphy <brad.murphy@byhook.com>
2015-10-16 14:20:40 -0400

    Added browser flags to tell user to upgrade browser.

--- app/load.js
+++ app/load.js
@@ -4,24 +4,65 @@
  * and set up the single page app.
  */
 
-// config and app
+// browser, config, selectors
 var config = require('./config');
-var app = require('./app');
+var browser = require('bowser');
+var nope = document.querySelector('.nope');
+var wrap = document.querySelector('.wrap');
+var link = document.querySelector('.whoopsy a');
 
-// router
-var router = require('./router');
+// browser control
+if (browser.msie && browser.version <= 10) {
 
-// controllers
-var AboutCtrl = require('./controllers/AboutCtrl');
-var WorkCtrl = require('./controllers/WorkCtrl');
-var ContactCtrl = require('./controllers/ContactCtrl');
-var ProjectCtrl = require('./controllers/ProjectCtrl');
-var NavCtrl = require('./controllers/NavCtrl');
-var MapCtrl = require('./controllers/MapCtrl');
+  wrap.style.display = 'none';
+  nope.style.display = 'block';
+  link.href = 'http://windows.microsoft.com/en-us/internet-explorer/download-ie';
 
-// load images
-var Loader = require('./core/Loader');
-var loader = new Loader(config.manifest);
+} else if (browser.chrome && browser.version <= 44) {
 
-console.log('Easter Egg: Type "what" + enter (not in the console. ;])');
+  wrap.style.display = 'none';
+  nope.style.display = 'block';
+  link.href = 'http://www.google.com/chrome';
+
+} else if (browser.opera && browser.version <= 31) {
+
+  wrap.style.display = 'none';
+  nope.style.display = 'block';
+  link.href = 'http://www.opera.com';
+
+} else if (browser.safari && browser.version <= 8) {
+
+  wrap.style.display = 'none';
+  nope.style.display = 'block';
+  link.href = '';
+
+} else if (browser.firefox && browser.version <= 40) {
+
+  wrap.style.display = 'none';
+  nope.style.display = 'block';
+  link.href = 'http://www.mozilla.com/firefox';
+
+} else {
+
+  // app
+  var app = require('./app');
+
+  // router
+  var router = require('./router');
+
+  // controllers
+  var AboutCtrl = require('./controllers/AboutCtrl');
+  var WorkCtrl = require('./controllers/WorkCtrl');
+  var ContactCtrl = require('./controllers/ContactCtrl');
+  var ProjectCtrl = require('./controllers/ProjectCtrl');
+  var NavCtrl = require('./controllers/NavCtrl');
+  var MapCtrl = require('./controllers/MapCtrl');
+
+  // load images
+  var Loader = require('./core/Loader');
+  var loader = new Loader(config.manifest);
+
+  console.log('Easter Egg: Type "what" + enter (not in the console. ;])');
+
+}
 
