afa8cb0c46c29b0174ecf182e2fade5348dd91f3
Brad Murphy <brad.murphy@byhook.com>
2015-12-09 16:58:28 -0500

    Added in rewind data, assets and content.

--- app/config.js
+++ app/config.js
@@ -86,6 +86,27 @@ var config = {
       ]
     },
     {
+      name: 'youtube-rewind',
+      logo: './images/logo-youtube.png',
+      title: 'Rewind to the Future',
+      type: 'Website',
+      client: 'YouTube',
+      external: 'http://rewindtothefuture.withyoutube.com',
+      content: './content/projects/youtube-rewind.html',
+      date: 'Decenver 2015',
+      thumb: 'url(./images/thumb-youtube-rewind.jpg) no-repeat 50% 50%',
+      hero: 'url(./images/hero-youtube-rewind.jpg) no-repeat 50% 50%',
+      technology: [
+        'HTML5',
+        'CSS3',
+        'Javascript',
+        'GSAP',
+        'Polymer',
+        'CreateJS',
+        'Underscore.js'
+      ]
+    },
+    {
       name: 'ok-google-masthead',
       logo: './images/logo-google.png',
       title: 'Ok Google',
